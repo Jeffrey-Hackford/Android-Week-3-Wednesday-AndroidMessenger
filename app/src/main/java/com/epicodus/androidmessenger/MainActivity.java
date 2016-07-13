e23b205017a32131ea6d47e21332d58e71b84ccc
Jeffrey Hackford and Kyle Wolfson <kyle@>
2016-07-13 16:21:38 -0700

    added user creation and firebase setup

---
+++ app/src/main/java/com/epicodus/androidmessenger/MainActivity.java
@@ -0,0 +1,32 @@
+package com.epicodus.androidmessenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.registerTextView) TextView mRegisterTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+        mRegisterTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mRegisterTextView) {
+            Intent intent = new Intent(MainActivity.this, CreateAccountActivity.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+}
